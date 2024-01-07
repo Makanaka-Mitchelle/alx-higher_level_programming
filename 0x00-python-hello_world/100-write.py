ec645ef2b38e3fb9efe00953645b82356ff2ad67
Makanaka-Mitchelle <chawiramitchelle@gmail.com>
2024-01-07 11:43:42 -0800

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import sys
 
 # Writing to stderr using sys.stderr.write
@@ -5,4 +7,5 @@ sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 
 # Exiting with status code 1
 sys.exit(1)
+
 
